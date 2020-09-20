1d196a9744cf47a1fc12be7bcdb436897cebfe9b
Hans Roche <hansrvi@gmail.com>
2020-09-19 23:45:25 -0600

    antes de implementar Boom

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const app = express();
 const { config } = require("./config/index");
 const moviesApi = require("./routes/movies.js");
 
+// middleware de errores
+const {
+	logErrors,
+	errorHandler,
+} = require("./utils/middleware/errorHandlers.js");
+
+// middleware del body parser
+app.use(express.json());
+
 moviesApi(app);
+
+// middleware de errores, siempre tienen que ir al final de las rutas
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
 	console.log(`Listening http://localhost:${config.port}`);
